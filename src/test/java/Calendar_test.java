396aaa17cfdce49f9543b4a1814841de92509335
lukashudec <lukas.hudec@centrum.sk>
2021-02-17 18:01:55 +0100

    unit test input cleanup

--- src/test/java/Calendar_test.java
+++ src/test/java/Calendar_test.java
@@ -5,61 +5,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Calendar_test {
+    Calendar calendar1 = new Calendar(new ArrayList<>(List.of
+            (new String[]{"09:00", "10:30"}, new String[]{"12:00", "13:00"}, new String[]{"16:00", "18:00"})), new String[]{"09:00", "20:00"});
+    Calendar calendar2 = new Calendar("09:00-10:30,12:00-13:00,16:00-18:00","09:00-20:00");
+    Calendar calendar3 = new Calendar("12:00-13:00,16:00-18:00","09:00-20:00");
+    Calendar calendar4 = new Calendar("10:00-11:45,12:30-14:30","10:00-18:30");
 
     @Test
     void test_to_string() {
-        Calendar cal1 = new Calendar(new ArrayList<>(List.of
-                (new String[]{"12:00", "13:00"}, new String[]{"16:00", "18:00"})),
-                new String[]{"09:00", "20:00"});
-        String result = "[[12:00, 13:00], [16:00, 18:00]] / [09:00, 20:00]";
-        assertEquals(result, cal1.toString(), "Not equal");
+        String result = "[[09:00, 10:30], [12:00, 13:00], [16:00, 18:00]] / [09:00, 20:00]";
+        assertEquals(result, calendar1.toString(), "Not equal");
     }
 
     @Test
     void test_calendar_creation() {
-        Calendar cal1 = new Calendar(new ArrayList<>(List.of
-                (new String[]{"09:00", "10:30"}, new String[]{"12:00", "13:00"}, new String[]{"16:00", "18:00"})), new String[]{"09:00", "20:00"});
-        Calendar cal2 = new Calendar("09:00-10:30,12:00-13:00,16:00-18:00","09:00-20:00");
-        assertEquals(cal1.toString(),cal2.toString(), "Not equal");
+        assertEquals(calendar1.toString(),calendar2.toString(), "Not equal");
     }
 
     @Test
     void test_calendar_equality() {
-        Calendar cal1 = new Calendar(new ArrayList<>(List.of
-                (new String[]{"09:00", "10:30"}, new String[]{"12:00", "13:00"}, new String[]{"16:00", "18:00"})), new String[]{"09:00", "20:00"});
-        Calendar cal2 = new Calendar("09:00-10:30,12:00-13:00,16:00-18:00","09:00-20:00");
-        assertEquals(cal2, cal1, "Not equal");
-        assertEquals(cal1.getCalendar(), cal2.getCalendar(), "Not equal" );
+        assertEquals(calendar2, calendar1, "Not equal");
+        assertEquals(calendar1.getCalendar(), calendar2.getCalendar(), "Not equal" );
     }
 
     @Test
     void test_calendar_merge() {
-        Calendar cal1 = new Calendar(new ArrayList<>(List.of
-                (new String[]{"12:00", "13:00"}, new String[]{"16:00", "18:00"})),
-                new String[]{"09:00", "20:00"});
-        Calendar cal2 = new Calendar(new ArrayList<>(List.of
-                (new String[]{"10:00", "11:45"}, new String[]{"12:30", "14:30"})),
-                new String[]{"10:00", "18:30"});
-        Calendar result = new Calendar(new ArrayList<>(List.of
-                (new String[]{"10:00", "11:45"}, new String[]{"12:00", "13:00"},new String[]{"12:30", "14:30"}, new String[]{"16:00", "18:00"})),
-                new String[]{"10:00", "18:30"});
-        Calendar cal3 = cal1.mergeWithCalendar(cal2);
-        assertEquals(cal3.toString(), result.toString(),"Not equal");
+        Calendar result = new Calendar("10:00-11:45,12:00-13:00,12:30-14:30,16:00-18:00","10:00-18:30");
+        assertEquals(calendar3.mergeWithCalendar(calendar4).toString(), result.toString(),"Not equal");
     }
 
     @Test
     void test_calendar_get_possible_events_with() {
-        Calendar cal1 = new Calendar(new ArrayList<>(List.of
-                (new String[]{"12:00", "13:00"}, new String[]{"16:00", "18:00"})),
-                new String[]{"09:00", "20:00"});
-        Calendar cal2 = new Calendar(new ArrayList<>(List.of
-                (new String[]{"10:00", "11:45"}, new String[]{"12:30", "14:30"})),
-                new String[]{"10:00", "18:30"});
-        Calendar result = new Calendar(new ArrayList<>(List.of
-                (new String[]{"10:00", "11:45"}, new String[]{"12:00", "13:00"},new String[]{"12:30", "14:30"}, new String[]{"16:00", "18:00"})),
-                new String[]{"10:00", "18:30"});
-        List<Integer[]> cal3 = cal1.mergeWithCalendar(cal2).getFreeTime();
-        List<Integer[]> cal4 = cal1.getPossibleEvents(cal2, 30);
+        List<Integer[]> cal3 = calendar3.mergeWithCalendar(calendar4).getFreeTime();
+        List<Integer[]> cal4 = calendar3.getPossibleEvents(calendar4, 30);
         assertArrayEquals(cal3.toArray(), cal4.toArray(),"Not equal");
     }
 
