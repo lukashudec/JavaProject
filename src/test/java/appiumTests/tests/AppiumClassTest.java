130c972c4a5e672b06f6aaf4c3db0b1bfae875b2
lukashudec <lukas.hudec@centrum.sk>
2021-03-24 16:24:16 +0100

    auto emulator + appium runner

--- src/test/java/appiumTests/tests/AppiumClassTest.java
+++ src/test/java/appiumTests/tests/AppiumClassTest.java
@@ -3,27 +3,55 @@ package appiumTests.tests;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
-
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 class AppiumClassTest {
 
-    AppiumDriver<MobileElement> driver;
+    static AppiumDriver<MobileElement> driver;
+    static AppiumDriverLocalService server;
+    static Process process;
+
+    @BeforeAll
+    public static void startEmulatorAndAppium() throws InterruptedException, IOException {
+        process = new ProcessBuilder("C:/Users/lenovo/AppData/Local/Android/Sdk/emulator/emulator.exe", "-avd", "Gala_11", "no-snapshot-save").start();
+        process.waitFor(5, TimeUnit.SECONDS);
+
+
+        HashMap<String, String> environment = new HashMap<>();
+        environment.put("PATH", "/usr/local/bin:" + System.getenv("PATH"));
+
+        AppiumServiceBuilder serviceBuilder = new AppiumServiceBuilder()
+                .usingAnyFreePort()
+                .withEnvironment(environment);
+
+        server = AppiumDriverLocalService.buildService(serviceBuilder);
+
+        if (process.isAlive()) {
+         server.start(); }
+    }
+
+    @AfterAll
+    public static void stopEmulatorAndAppium() {
+        server.stop();
+        process.destroy();
+    }
+
 
     @BeforeEach
-    public void setUp() throws MalformedURLException {
+    public void setUp()  {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "android");
-        driver = new AppiumDriver<>(new URL(" http://127.0.0.1:4723/wd/hub"), caps);
+        driver = new AppiumDriver<>(server.getUrl(), caps);
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
     }
 
