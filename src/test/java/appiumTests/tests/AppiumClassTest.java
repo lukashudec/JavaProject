62da73e34c0e18a903e2bb96b1880ae958606f58
lukashudec <lukas.hudec@centrum.sk>
2021-03-25 14:25:35 +0100

    first appium tests (no assertions ) added appium / emulator restart after each test - slows things down but resets device state to last snapshot

--- src/test/java/appiumTests/tests/AppiumClassTest.java
+++ src/test/java/appiumTests/tests/AppiumClassTest.java
@@ -1,12 +1,12 @@
 package appiumTests.tests;
 
-import appiumTests.tests.pages.AlarmPage;
 import appiumTests.tests.pages.MainPage;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.service.local.flags.GeneralServerFlag;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.AfterEach;
@@ -23,78 +23,85 @@ class AppiumClassTest {
     static AppiumDriverLocalService server;
     static Process process;
 
+    public static void startEmulator() throws IOException {
+        process = new ProcessBuilder("C:/Users/lenovo/AppData/Local/Android/Sdk/emulator/emulator.exe", "-avd", "Gala_11", "-no-snapshot-save").start();
+    }
+
     @BeforeAll
-    public static void startEmulatorAndAppium() {
-        System.out.println("Starting emulator");
-        try {
-            process = new ProcessBuilder("C:/Users/lenovo/AppData/Local/Android/Sdk/emulator/emulator.exe", "-avd", "Gala_11", "-no-snapshot-save").start();
-            process.waitFor(5, TimeUnit.SECONDS);
-        }
-        catch (InterruptedException | IOException e) {
-            System.out.println("Server goes oops");
-            e.printStackTrace();
-        }
+    public static void startEmulatorAndAppium() throws IOException {
+        startEmulator();
 
         HashMap<String, String> environment = new HashMap<>();
         environment.put("PATH", "/usr/local/bin:" + System.getenv("PATH"));
 
-        System.out.println("Starting appium");
         AppiumServiceBuilder serviceBuilder = new AppiumServiceBuilder()
                 .usingAnyFreePort()
-                .withEnvironment(environment);
+                .withEnvironment(environment)
+                .withArgument(GeneralServerFlag.LOG_LEVEL,"error");
 
         server = AppiumDriverLocalService.buildService(serviceBuilder);
-
-        if (process.isAlive()) {
-            System.out.println("Starting server");
-         server.start(); }
+        server.start();
     }
 
     @AfterAll
-    public static void stopEmulatorAndAppium() {
+    public static void stopEmulatorAndAppium() throws InterruptedException {
         server.stop();
-        process.destroy();
+        process.children().forEach(ProcessHandle::destroy);
+        process.destroyForcibly().waitFor();
     }
 
 
     @BeforeEach
-    public void setUp()  {
+    public void setUp() throws IOException {
+        if (!process.isAlive()) {
+            System.out.println("Is alive");
+            startEmulator();
+            server.start();
+        }
+
         System.out.println("Setting up");
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "android");
         driver = new AppiumDriver<>(server.getUrl(), caps);
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
     }
 
     @AfterEach
-    public void tearDown() {
+    public void tearDown() throws InterruptedException {
          if (driver != null) {
             driver.quit();
+            server.stop();
+            process.children().forEach(ProcessHandle::destroy);
+            process.destroyForcibly().waitFor();
+
         }
     }
 
     @Test
-    void testAppium() throws InterruptedException {
-        MainPage page = new MainPage(driver);
-        AlarmPage alarmPage = page.visit().goToAlarm();
-        Thread.sleep(1500);
+    void testAppium() {
+        new MainPage(driver)
+                .visit()
+                .goToAlarm()
+                .expandAlarm(1)
+                .clickOnDay("Monday")
+                .clickOnDay("Tuesday")
+                .clickOnDay("Wednesday");
+    }
 
-      /*  driver.findElementByAccessibilityId("Clock").click();
+    @Test
+    void testCreateAlarm() {
+        new MainPage(driver)
+                .visit()
+                .goToAlarm()
+                .createNewAlarm(12,30, new String[]{"Monday", "Friday"});
+    }
 
-        driver.findElementByAccessibilityId("Alarm").click();
-        driver.findElementByAccessibilityId("Clock").click();
-        driver.findElementByAccessibilityId("Timer").click();
-        driver.findElementByAccessibilityId("StopWatch").click();
-
-        driver.findElementByXPath("(//android.view.ViewGroup[@content-desc=\" Alarm\"])[2]").click();
-        driver.findElementByAccessibilityId("Monday").click();
-        driver.findElementByAccessibilityId("9:00AM").click();
-        driver.findElementByAccessibilityId("11").click();
-        driver.findElementById("android:id/button1").click();
-        driver.findElementByAccessibilityId("Collapse alarm").click();
-
-        driver.findElementByXPath("//android.view.ViewGroup[@content-desc=\" Alarm\"])[2]/android.widget.Switch").click();
-   */
+    @Test
+    void testDeleteAlarm() {
+        new MainPage(driver)
+                .visit()
+                .goToAlarm()
+                .deleteAlarm(2);
     }
 
 
--- src/test/java/appiumTests/tests/pages/AlarmPage.java
+++ src/test/java/appiumTests/tests/pages/AlarmPage.java
@@ -8,7 +8,58 @@ public class AlarmPage extends MainPage {
     @AndroidFindBy(accessibility = "Alarm")
     protected MobileElement rootLocator;
 
+
     public AlarmPage(AppiumDriver<MobileElement> appiumDriver) {
         super(appiumDriver);
     }
+
+    public AlarmPage visit() {
+        rootLocator.click();
+        return this;
+    }
+
+    public AlarmPage expandAlarm(int alarmRow) {
+        driver.findElementByXPath("(//android.view.ViewGroup[@content-desc=\" Alarm\"])["+alarmRow+"]")
+                .click();
+        return this;
+    }
+
+    public AlarmPage clickOnDay(String day) {
+        driver.findElementByAccessibilityId(day).click();
+        return this;
+    }
+
+    public AlarmPage clickOnHour(int hour) {
+        driver.findElementByAccessibilityId(String.valueOf(hour)).click();
+        return this;
+    }
+
+    public AlarmPage createNewAlarm(int hour, int minute,String[] days) {
+        driver.findElementByAccessibilityId("Add alarm").click();
+        // hour , minute
+        driver.findElementByAccessibilityId("12").click();
+        driver.findElementByAccessibilityId("30").click();
+        // ok hour
+        driver.findElementById("android:id/button1").click();
+        // repeat checkbox
+        driver.findElementById("com.google.android.deskclock:id/repeat_onoff").click();
+
+        this.clickOnDay("Monday");
+        this.clickOnDay("Friday");
+        return this;
+    }
+
+    public AlarmPage deleteAlarm(int row) {
+        this.expandAlarm(row);
+        driver.findElementById("com.google.android.deskclock:id/delete").click();
+        return this;
+    }
+
+    public boolean isExtended() {
+        return driver.findElementsByAccessibilityId("Collapse alarm").size() != 0;
+    }
+
+
+
+
 }
