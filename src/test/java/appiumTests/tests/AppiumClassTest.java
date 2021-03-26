00a490d50c7fe512378e23ece4cedcbab63f2748
lukashudec <lukas.hudec@centrum.sk>
2021-03-26 16:22:45 +0100

    trying to speed up appium startup

--- src/test/java/appiumTests/tests/AppiumClassTest.java
+++ src/test/java/appiumTests/tests/AppiumClassTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-import com.google.common.base.Stopwatch;
+
 
 class AppiumClassTest {
 
@@ -19,37 +19,17 @@ class AppiumClassTest {
     static AppiumDriverLocalService server;
     static Process process;
 
-    public static void startEmulator() throws IOException {
-        process = new ProcessBuilder("C:/Users/lenovo/AppData/Local/Android/Sdk/emulator/emulator.exe", "-avd", "Gala_11", "-no-snapshot-save")
-                .start();
-    }
 
     @BeforeAll
-    public static void startEmulatorAndAppium() throws IOException {
-        startEmulator();
+    static void setUp() throws IOException {
+        process = new ProcessBuilder("C:/Users/lenovo/AppData/Local/Android/Sdk/emulator/emulator.exe", "-avd", "Gala_11", "-no-snapshot-save")
+                .start();
 
         AppiumServiceBuilder serviceBuilder = new AppiumServiceBuilder()
                 .usingAnyFreePort()
                 .withArgument(GeneralServerFlag.LOG_LEVEL,"error");
-
         server = AppiumDriverLocalService.buildService(serviceBuilder);
         server.start();
-    }
-
-    @AfterAll
-    public static void stopEmulatorAndAppium() throws InterruptedException {
-        server.stop();
-        process.children().forEach(ProcessHandle::destroy);
-        process.destroyForcibly().waitFor();
-    }
-
-
-    @BeforeEach
-    public void setUp() throws IOException {
-        if (!process.isAlive()) {
-            startEmulator();
-            server.start();
-        }
 
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "android");
@@ -59,24 +39,25 @@ class AppiumClassTest {
         caps.setCapability("appActivity", "com.android.deskclock.DeskClock");
         driver = new AppiumDriver<>(server.getUrl(), caps);
         driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
-
     }
 
     @AfterEach
-    public void tearDown() throws InterruptedException {
-        Stopwatch timer = Stopwatch.createStarted();
-         if (driver != null) {
-            driver.quit();
-            server.stop();
-            process.children().forEach(ProcessHandle::destroy);
-            process.destroyForcibly().waitFor();
-        }
-        System.out.println("AfterEach duration : "+timer.stop());
+    public void tearDown() {
+        driver.resetApp();
+
     }
+
+    @AfterAll
+    static void close() throws InterruptedException {
+        driver.quit();
+        server.stop();
+        process.children().forEach(ProcessHandle::destroy);
+        process.destroyForcibly().waitFor();
+    }
+
         @Test
         void testAppium() {
         // this test can be remade and addted to CreateDelete -> CreateDeleteEdit to save additional time on test running
-            Stopwatch timer = Stopwatch.createStarted();
             new MainPage(driver)
                     .visit()
                     .goToAlarm()
@@ -84,12 +65,10 @@ class AppiumClassTest {
                     .clickOnDay("Monday")
                     .clickOnDay("Tuesday")
                     .clickOnDay("Wednesday");
-            System.out.println("Test body : "+timer.stop());
         }
 
         @Test
         void testCreateDeleteAlarm() {
-            Stopwatch timer = Stopwatch.createStarted();
         /* two tests combined into one
         * starting appium,emulator,driver is time consuming so it should be better to write more complex tests
         * even though writing complex tests is antipattern */
@@ -99,7 +78,6 @@ class AppiumClassTest {
                     .createNewAlarm(12, 30, new String[]{"Monday", "Friday"})
                     .goToAlarm()
                     .deleteAlarm(1);
-            System.out.println("Test body : "+timer.stop());
         }
 
 }
