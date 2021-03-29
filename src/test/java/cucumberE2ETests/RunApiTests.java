87f9fad63376517422f0675640b2c484028cb49f
lukashudec <lukas.hudec@centrum.sk>
2021-03-29 23:51:05 +0200

    trying to run allure for jUnit4 and jUnit5

--- src/test/java/cucumberE2ETests/RunApiTests.java
+++ src/test/java/cucumberE2ETests/RunApiTests.java
@@ -4,7 +4,6 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty"},
         features = "src/test/java/cucumberE2ETests/Features/API",
