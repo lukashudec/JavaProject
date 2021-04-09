a083f615089850e66e58d5796811ebed6916d488
lukashudec <lukas.hudec@centrum.sk>
2021-04-09 13:30:33 +0200

    simplified timeTuple squish updated unitTests

--- src/main/java/calendar/utility/TimeTuple.java
+++ src/main/java/calendar/utility/TimeTuple.java
@@ -3,20 +3,20 @@ package calendar.utility;
 import java.util.Objects;
 
 public class TimeTuple {
-    private String fromStr;
-    private int fromInt;
-    private String toStr;
-    private int toInt;
+    private String strFrom;
+    private int intFrom;
+    private String strTo;
+    private int intTo;
 
     public TimeTuple(String s) {
         this(s.split("-")[0],s.split("-")[1]);
     }
 
     public TimeTuple(String from, String to) {
-        this.fromStr = from;
-        fromInt = convertToInt(from);
-        this.toStr = to;
-        toInt = convertToInt(to);
+        this.strFrom = from;
+        intFrom = convertToInt(from);
+        this.strTo = to;
+        intTo = convertToInt(to);
     }
 
     private int convertToInt(String s) {
@@ -24,51 +24,56 @@ public class TimeTuple {
     }
 
     public void setFrom(String from) {
-        this.fromStr = from;
-        fromInt = convertToInt(from);
+        this.strFrom = from;
+        intFrom = convertToInt(from);
     }
 
     public void setTo(String to) {
-        this.toStr = to;
-        toInt = convertToInt(to);
+        this.strTo = to;
+        intTo = convertToInt(to);
     }
 
-    public static TimeTuple[] sort(TimeTuple one, TimeTuple two) {
-        if (one.fromInt<=two.fromInt) {
-            return new TimeTuple[]{one,two};
-        } else return new TimeTuple[]{two,one};
+
+    public static TimeTuple[] sort(TimeTuple one, TimeTuple two,String key) {
+        if (key.equals("FROM")) {
+            if (one.intFrom <=two.intFrom) {
+                return new TimeTuple[]{one,two};
+            } else return new TimeTuple[]{two,one}; }
+        else if (key.equals("TO")) {
+            if (one.intTo <=two.intTo) {
+                return new TimeTuple[]{one,two};
+            } else return new TimeTuple[]{two,one}; }
+        return new TimeTuple[]{one,two};
     }
 
     public TimeTuple squish(TimeTuple t) {
-        TimeTuple first = sort(this,t)[0];
-        TimeTuple second = sort(this,t)[1];
+        TimeTuple first = sort(this,t,"FROM")[0];
+        TimeTuple second = sort(this,t,"FROM")[1];
 
-        if (first.toInt>=second.toInt) {
-            return first;
-        } else if(first.toInt>=second.fromInt) {
-            first.setTo(second.toStr);
-            return first;
+        if (second.intFrom>=first.intFrom && second.intFrom<=first.intTo) {
+            return new TimeTuple(first.strFrom,sort(first,second,"TO")[1].strTo);
         }
+
         return null;
     }
 
     @Override
     public String toString() {
-        return fromInt +"-"+ toInt;
+        return intFrom +"-"+ intTo;
     }
 
     public String strFrom() {
-        return fromStr;
+        return strFrom;
     }
 
     public int intFrom() {
-        return fromInt;
+        return intFrom;
     }
 
-    public String strTo() { return toStr; }
+    public String strTo() { return strTo; }
 
     public int intTo() {
-        return toInt;
+        return intTo;
     }
 
     @Override
@@ -76,11 +81,11 @@ public class TimeTuple {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         TimeTuple timeTuple = (TimeTuple) o;
-        return fromStr.equals(timeTuple.fromStr) && toStr.equals(timeTuple.toStr);
+        return strFrom.equals(timeTuple.strFrom) && strTo.equals(timeTuple.strTo);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(fromStr, toStr);
+        return Objects.hash(strFrom, strTo);
     }
 }
--- src/test/java/unitTests/utility/TimeTupleTest.java
+++ src/test/java/unitTests/utility/TimeTupleTest.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class TimeTupleTest {
     TimeTuple t1 = new TimeTuple("12:00-13:00");
     TimeTuple t2 = new TimeTuple("12:00","13:00");
+    TimeTuple t3 = new TimeTuple("14:00","15:00");
 
     @Test
     void testEquals() {
@@ -25,33 +26,59 @@ class TimeTupleTest {
     }
 
     @Test
-    void strFrom() {
+    void testStrFrom() {
         assertEquals("12:00",t1.strFrom());
     }
 
     @Test
-    void intFrom() {
+    void testIntFrom() {
         assertEquals(720,t1.intFrom());
     }
 
     @Test
-    void strTo() {
+    void testStrTo() {
         assertEquals("13:00",t1.strTo());
     }
 
     @Test
-    void intTo() {
+    void testIntTo() {
         assertEquals(780,t1.intTo());
     }
 
     @Test
-    void sort() {
-        TimeTuple t1 = new TimeTuple("12:00-13:00");
-        TimeTuple t2 = new TimeTuple("14:00","15:00");
-
-        assertEquals(t1,TimeTuple.sort(t2,t1)[0]);
-        assertEquals(t2,TimeTuple.sort(t2,t1)[1]);
+    void testSortFrom() {
+        assertEquals(t1,TimeTuple.sort(t3,t1,"FROM")[0]);
+        assertEquals(t3,TimeTuple.sort(t3,t1,"FROM")[1]);
     }
+
+    @Test
+    void testSortTo() {
+        assertEquals(t1,TimeTuple.sort(t3,t1,"TO")[0]);
+        assertEquals(t3,TimeTuple.sort(t3,t1,"TO")[1]);
+    }
+
+    @Test
+    void testSquishBorder() {
+        TimeTuple t1 = new TimeTuple("12:00-14:00");
+        TimeTuple t2 = new TimeTuple("14:00","15:00");
+        assertEquals(new TimeTuple("12:00","15:00"),t1.squish(t2));
+
+    }
+
+    @Test
+    void testSquishOverlap() {
+        TimeTuple t1 = new TimeTuple("12:00-14:30");
+        TimeTuple t2 = new TimeTuple("14:00","15:00");
+        assertEquals(new TimeTuple("12:00","15:00"),t1.squish(t2));
+    }
+
+    @Test
+    void testSquishInside() {
+        TimeTuple t1 = new TimeTuple("12:00-15:00");
+        TimeTuple t2 = new TimeTuple("13:00","14:00");
+        assertEquals(new TimeTuple("12:00","15:00"),t1.squish(t2));
+    }
+
 
 
 }
